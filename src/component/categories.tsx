5c9a80a79ffadab09b6d53ebab07b48aef80ba9d
Dilhan Kösekul <dilhan.kosekul@gmail.com>
2024-10-02 15:44:16 +0300

    add catagory page buttons

--- src/component/categories.tsx
+++ src/component/categories.tsx
@@ -1,5 +1,5 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material"
-import { Box } from "@mui/system"
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Box, padding } from "@mui/system"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
@@ -12,9 +12,6 @@ const categories = () => {
   }
   )
 
-
-
-
   return (
     <>
       <TableContainer>
@@ -23,15 +20,31 @@ const categories = () => {
             <TableRow>
               <TableCell>ADI</TableCell>
               <TableCell>DURUMU</TableCell>
+              <TableCell>iŞLEMLER</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-{categoryList.map((item:any) =>(
-<TableRow>
-  <TableCell>{item.name}</TableCell>
-  <TableCell>{item.state ==  true ? 'Stokta ': 'Stok Yok' }</TableCell>
-</TableRow>
-))}
+            {categoryList.map((item: any) => (
+              <TableRow>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.state == true ? 'Stokta ' : 'Stok Yok'}</TableCell>
+                <Box display="flex" gap={1} 
+                 style={{ padding:3}}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                  >Düzenle</Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                  >Kaydet</Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                  >Sil</Button>
+                </Box>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
